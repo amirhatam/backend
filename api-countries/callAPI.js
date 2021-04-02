78150be69699ae08605c0b9c8ade8c5a376e4950
Amir <amirhatam136@gmail.com>
2021-04-03 00:41:55 +0200

    fini

--- api-countries/callAPI.js
+++ api-countries/callAPI.js
@@ -1,7 +1,15 @@
 var request = require("request");
 
 request.get("http://localhost:8000/countries", function (err, res, body) {
-    console.log(err);
-    console.log(res.statusCode);
-    console.log(body);
+
+
+
+    var cnt = JSON.parse(body);
+    var cntN = cnt.map(function (elem) {
+        return elem
+    })
+
+
+    console.log(cntN.reverse());
 });
+
--- api-countries/index.js
+++ api-countries/index.js
@@ -1,30 +1,16 @@
-// const express = require('express');
-// const app = express();
-
-// const port = 8000;
-
-// app.get('/countries ', (req, res) => {
-//     res.send(["Italy", "USA", "Inde", "UK", "Spain"]);
-// });
-
-
-// app.listen(port, () => {
-//     console.log('Server started on port: ' + port);
-// });
-
-
-
-
 
 const express = require('express');
 const port = 8000;
 
 const app = express();
 
+var cors = require('cors')
+app.use(cors())
+
 
 // Routes
 app.get('/countries', (req, res) => {
-    res.send(["Italy", "USA", "Inde", "UK", "Spain"]);
+    res.send(["Italy ", " Cuba ", " Japan ", " Australia ", " Spain"]);
 });
 
 
