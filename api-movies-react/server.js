ad0aa340172e2921987c71d0b1c46ce50dcbe059
Amir <amirhatam136@gmail.com>
2021-05-25 16:52:29 +0200

    moovice backend

---
+++ api-movies-react/server.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const cors = require("cors")
+const dataMoviesPopulair = require("./dataMoviesPopulair.js")
+const dataMoviesWeekly = require("./dataMoviesWeekly.js")
+
+const app = express()
+
+app.use(cors())
+
+const port = 8000
+
+
+// app.get("/movies/", (req,res) => {
+
+//     res.json({
+//         dataMoviesPopulair
+//     })
+// })
+
+app.get('/movies/populair', (req, res) => {
+    res.send(dataMoviesPopulair);
+  });
+
+app.get('/movies/weekly', (req, res) => {
+    res.send(dataMoviesWeekly);
+  });
+
+
+// app.get("/movies/title/:movie", (req,res) => {
+//     const movie = req.params.movie.toUpperCase()
+//     let resultMovies= []
+
+//     for (let index = 0; index < dataMovies.length; index++) {
+//         const curmovies = dataMovies[index];
+
+//         if (curmovies.title.toUpperCase() === movie) {
+//             resultMovies.push(curmovies)
+//             // resultCountries=curPays
+
+//         }
+//     }
+//     // const country = req.params.team.toLocaleUpperCase()
+//     res.json({
+//         resultMovies
+//     })
+// })
+
+
+app.listen(port, () => {
+    console.log("Server à l'écoute dans le port 8000");
+})
