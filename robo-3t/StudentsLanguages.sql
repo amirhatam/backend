00771a0ef3098e1f60019c8955ad8a9924ecdabb
Amir <amirhatam136@gmail.com>
2021-06-04 15:54:36 +0200

    ex-students-lang

---
+++ robo-3t/StudentsLanguages.sql
@@ -0,0 +1,43 @@
+use students_languages
+
+db.students_languages.insert(
+{StudentId: 1, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 1, LanguageId: 2}
+)
+db.students_languages.insert(
+{StudentId: 2, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 2, LanguageId: 3}
+)
+db.students_languages.insert(
+{StudentId: 3, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 4, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 4, LanguageId: 4}
+)
+db.students_languages.insert(
+{StudentId: 4, LanguageId: 5}
+)
+db.students_languages.insert(
+{StudentId: 5, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 5, LanguageId: 5}
+)
+db.students_languages.insert(
+{StudentId: 6, LanguageId: 1}
+)
+db.students_languages.insert(
+{StudentId: 6, LanguageId: 2}
+)
+db.students_languages.insert(
+{StudentId: 6, LanguageId: 3}
+)
+
+db.students_languages.find()
