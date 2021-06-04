4e1d990970a6b3d337e05d5bcb54a9658c3457e1
Amir <amirhatam136@gmail.com>
2021-06-04 14:48:44 +0200

    ex-students

---
+++ robo-3t/students.sql
@@ -0,0 +1,20 @@
+use school
+
+db.students.insert(
+{name: "Véronique", city: "Paris"}
+)
+db.students.insert(
+{name: "Steeven", city: "Lyon"}
+)
+db.students.insert(
+{name: "Marc", city: "Marseille"}
+)
+db.students.insert(
+{name: "Nour", city: "Lyon"}
+)
+db.students.insert(
+{name: "Romain", city: "Paris"}
+)
+db.students.insert(
+{name: "Sophie", city: "Paris"}
+)
