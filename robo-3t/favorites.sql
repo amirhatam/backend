f2c1e5b8e2a773da041f258e21b61127b66c562f
Amir <amirhatam136@gmail.com>
2021-06-04 15:08:36 +0200

    ex-fav

---
+++ robo-3t/favorites.sql
@@ -0,0 +1,22 @@
+use favorites
+
+db.favorites.insert(
+{Class: "Maths", Sport: "Cricket", StudentId: 4}
+)
+db.favorites.insert(
+{Class: "Music", Sport: "Hip-Hop", StudentId: 2}
+)
+db.favorites.insert(
+{Class: "Arts", Sport: "Boxing", StudentId: 6}
+)
+db.favorites.insert(
+{Class: "Literature", Sport: "Tennis", StudentId: 1}
+)
+db.favorites.insert(
+{Class: "Computer science", Sport: "Tennis", StudentId: 3}
+)
+db.favorites.insert(
+{Class: "Arts", Sport: "Baseball", StudentId: 5}
+)
+
+db.favorites.find()
