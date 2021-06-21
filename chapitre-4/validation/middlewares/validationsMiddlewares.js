ee5cf3ab611740c1098b5334098f96840e08d0ef
Amir <amirhatam136@gmail.com>
2021-06-21 17:18:25 +0200

    last commit

--- chapitre-4/validation/middlewares/validationsMiddlewares.js
+++ chapitre-4/validation/middlewares/validationsMiddlewares.js
@@ -2,9 +2,13 @@ const expressValidator = require("express-validator");
 
 const validationUsers = [
     expressValidator.body("username").isString(),
-    expressValidator.body("email").isString(),
+    expressValidator.body("email").isEmail(),
     expressValidator.body("age").isInt().isLength({ min: 2, max: 3 }),
-    expressValidator.body("city").isString(),
+    expressValidator.body("city").custom(value => {
+        if(value === "Paris" || value === "Los Angeles" || value === "Tokyo") {
+            return value
+        }
+    })
 ]
 
 module.exports = {
